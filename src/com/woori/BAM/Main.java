0673a52bd32ebba6027df95ba00cd6ddfe099653
bot182-pixel <djong1023@gmail.com>
2025-12-01 17:57:05 +0900

    article no.2

--- src/com/woori/BAM/Main.java
+++ src/com/woori/BAM/Main.java
@@ -1,5 +1,8 @@
 package com.woori.BAM;
 
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -7,9 +10,12 @@ public class Main {
         System.out.println("==프로그램 시작==");
         Scanner in = new Scanner(System.in);
         int id = 1;                 //게시글의 번호를 저장한다
+
+        List<Article> articles = new ArrayList<>();    // 변수 articles 의 타입 --> 제너릭 <article>
+
         while (true) {
-            System.out.printf("명령어) ");
-            String cmd = in.nextLine();
+            System.out.printf("cmd) ");
+            String cmd = in.nextLine().trim();
             System.out.println("명령어) " + cmd);
 
             if (cmd.length() == 0) {
@@ -18,14 +24,33 @@ public class Main {
             }
 
             if (cmd.equals("article list")) {
-                System.out.println("게시글이 없습니다");
+                if (articles.size() == 0) {
+                    System.out.println("게시글이 없습니다");
+                    continue;
+                }
+
+                System.out.println("번호   |   제목");
+                for (int i = 0 ; i < articles.size() ; i++) {
+                    Article article = articles.get(i);
+                    System.out.printf("%d   |   %s\n" , article.id, article.title);
+                }
+
+
             } else if (cmd.equals("article write")) {
-                System.out.print("제목 :");
-                String title = in.nextLine().trim();    //trim(); 앞뒤의 공백을 제거
-                System.out.println("내용 :");
+                System.out.print("제목 : ");
+                String title = in.nextLine();
+                System.out.println("내용 : ");
                 String body = in.nextLine();
                 System.out.println(id + " 번글이 생성되었습니다");
+
+                Article article = new Article();
+                article.id = id;
+                article.title = title;
+                article.body = body;
+
+                articles.add(article);
                 id++;
+
             } else {
                 System.out.println("존재하지 않는 명령어입니다.");
             }
@@ -33,9 +58,19 @@ public class Main {
             if (cmd.equals("exit")) {
                 break;
             }
+
+//            System.out.println("id :"+ id);
+//            System.out.println("title : " + title);
+//            System.out.println("body : " + body);
         }
 
-        String cmd = in.nextLine();
         System.out.println("==프로그램 종료==");
     }
+}
+
+class Article {
+    int id;
+    String title;
+    String body;
+
 }
